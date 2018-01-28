cc19f35f6a4b013e9365b9f2636da868f7ee9311
Sobol-R <rostislav.sobolev@gmail.com>
2018-01-29 01:17:06 +0300

    add some test locations 0.2

--- src/Cafes.java
+++ src/Cafes.java
@@ -6,5 +6,6 @@ public class Cafes {
         bot.sendPhoto("https://lh5.googleusercontent.com/proxy/mTV2WV9pY16-fTbiphLIh54zWiQsG3iSReGnUX_RluA_o6gwN9EKh6aE65MBNKd43MPwA_HVvDuVJ6hrfiDA7y8JbbvNCLGcHfu6PtICe3GOmz15ubZfFOH2ET7xmCZvTvnPlmvdkiSVmjR8I13xL8YzyGfaPVs=w408-h271-k-no", chatid);
         bot.sendPhoto("https://st0.restorating.ru/upload/images/2017/01/10/07e09654ec.jpg", chatid);
         bot.sendPhoto("https://st0.restorating.ru/upload/images/2017/01/10/682ab8b54a.jpg", chatid);
+        bot.sendText("Более подробную информацию вы сможите найти на сайте ресторана: http://fermabenua.ru/", chatid);
     }
 }
